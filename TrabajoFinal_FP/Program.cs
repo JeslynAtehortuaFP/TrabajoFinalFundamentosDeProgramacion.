bc16092b4734189ebd33b1e4ede01c3a70c0d36b
Jeslyn Atehortua Henao <Jeslyn.atehortua@upb.edu.co>
2025-10-22 14:57:34 -0500

    Primer commit

---
+++ TrabajoFinal_FP/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TrabajoFinal_FP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
